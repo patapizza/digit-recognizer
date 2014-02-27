98cf2265c4517e287a17a4e8c5f0d0a874386e34
patapizza <patapizza@gmail.com>
2014-02-26 17:56:04 -0800

    backpropagation using stochastic gradient descent with cross-entropy error

--- rprop.R
+++ rprop.R
@@ -4,33 +4,46 @@ codebooks <- diag(10)
 
 train <- function(path) {
     data <- read.csv(path)
-    network <- list(b_input=vector(), w_input_h1=matrix(vector(), nrow=784, ncol=500), b_h1=vector(), w_h1_output=matrix(vector(), nrow=500, ncol=10))
+    network <- list(b_input=rnorm(784), w_input_h1=matrix(rnorm(784 * 500), nrow=784, ncol=500), b_h1=rnorm(500), w_h1_output=matrix(rnorm(500 * 10), nrow=500, ncol=10))
     rprop(network, data)
 }
 
-rprop <- function(network, data) {
+rprop <- function(network, data, eta=0.1) {
+    mse <- Inf
     repeat {
         sse <- 0
         for (i in 1:nrow(data)) {
-            o <- fw_pass(data[i, ])
-            error <- (codebooks[data[i, 1], ] - o[[length(o)]]) ^ 2
-            sse <- sse + error
+            o <- fw_pass(network, data[i, ])
+            error <- o[[3]] - codebooks[data[i, "label"] + 1, ]
+            sse <- sse + sum(error ^ 2) / 2
+
+            delta_h1_output <- error
+            delta_input_h1 <- (network[["w_h1_output"]] * delta_h1_output) * o[[2]] * (1 - o[[2]])
+
+            network[["w_input_h1"]] <- -eta * delta_input_h1 * o[[1]] + network[["w_input_h1"]]
+            network[["w_h1_output"]] <- -eta * delta_h1_output * o[[2]] + network[["w_h1_output"]]
         }
-        if (sse / nrow(data) < 0.01)
+        if (sse / nrow(data) >= mse)
+            eta <- eta / 2
+        mse <- sse / nrow(data)
+        cat("MSE:", mse, "(", eta,")\n")
+        if (mse < 0.01)
             break
     }
 }
 
-fw_pass <- function(input) {
-    layer1 <- hidden_activations(input)
-    output <- hidden_activations(layer1)
-    return list(input, layer1, output)
+fw_pass <- function(network, input) {
+    layer1 <- hidden_activations(input, network[["b_input"]], network[["w_input_h1"]])
+    output <- hidden_activations(layer1, network[["b_h1"]], network[["w_h1_output"]])
+    list(input, layer1, output)
 }
 
 sigmoid <- function(x) {
-    return 1 / (1 + exp(-x))
+    1 / (1 + exp(-x))
 }
 
 hidden_activations <- function(o, b, w) {
-    return sigmoid(b + w * o)
+    sigmoid(b + w * o)
 }
+
+train("./data/train_tiny.csv")
